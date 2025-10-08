721b93c509868b4567213027492bc933dca11cbd
Pierre <73099815+pierrecvn@users.noreply.github.com>
2025-10-08 16:27:19 +0200

    001 - inverser l’affichage du triangle

---
+++ Q5.java
@@ -0,0 +1,20 @@
+public class Q5 {
+	public static void main(String[] args) {
+		if (args.length == 0) {
+			System.out.println("Usage: java Q5 N");
+			return;
+		}
+
+		int n = Integer.parseInt(args[0]);
+
+		for (int i = n; i >= 1; i--) {
+			for (int j = 1; j <= i; j++) {
+				System.out.print(j);
+				if (j < i) {
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+	}
+}
